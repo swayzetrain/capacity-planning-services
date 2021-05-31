13d814ad92ce6a5656602512152c13d008b79bab
Joe Castle <brocatles@gmail.com>
2021-05-30 23:59:07 -0400

    Cognitive complexity reduction for TeamCapacityService

--- src/main/java/tech/swayzetrain/capacity/api/service/team/TeamCapacityService.java
+++ src/main/java/tech/swayzetrain/capacity/api/service/team/TeamCapacityService.java
@@ -12,10 +12,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import tech.swayzetrain.capacity.common.enums.Role;
+import tech.swayzetrain.capacity.common.exception.CapacityProcessingException;
 import tech.swayzetrain.capacity.common.model.DailyCapacity;
 import tech.swayzetrain.capacity.common.model.MonthlyCapacity;
 import tech.swayzetrain.capacity.common.model.RoleCapacity;
 import tech.swayzetrain.capacity.common.model.Team;
+import tech.swayzetrain.capacity.common.model.TeamMemberCapacity;
 
 @Service
 public class TeamCapacityService {
@@ -46,32 +48,19 @@ public class TeamCapacityService {
 			.filter(tmc -> null == endDate || tmc.getDate().compareTo(endDate) <= 0)
 			.forEach(teamMemberCapacity -> {
 				List<DailyCapacity> dailyCapacityList = null;
-				Boolean dailyCapacityAdded = false;
 
-				for (RoleCapacity roleCapacity : roleCapacityList) {
-					if (roleCapacity.getRole().equals(teamMember.getRole())) {
-						dailyCapacityList = roleCapacity.getDailyCapacity();
-					}
+				dailyCapacityList = getDailyCapacityByRole(roleCapacityList, teamMember.getRole());
+				
+				if(null == dailyCapacityList) {
+					throw new CapacityProcessingException(String.format("Exception encountered when trying to build a capacity report for role, %s", teamMember.getRole().toString()));
 				}
-
-				for (DailyCapacity capacity : dailyCapacityList) {
-					if (capacity.getDate().isEqual(teamMemberCapacity.getDate())) {
-						capacity.setHours(capacity.getHours().add(teamMemberCapacity.getHours()));
-						dailyCapacityAdded = true;
-					}
-				}
-
-				if (Boolean.FALSE.equals(dailyCapacityAdded)) {
-					DailyCapacity dailyCapacity = new DailyCapacity();
-					dailyCapacity.setHours(teamMemberCapacity.getHours());
-					dailyCapacity.setDate(teamMemberCapacity.getDate());
-
-					dailyCapacityList.add(dailyCapacity);
-				}
+				
+				addCapacityToDailyRoleCapacity(dailyCapacityList, teamMemberCapacity);
 			}));
 
 		return new ResponseEntity<>(roleCapacityList, HttpStatus.OK);
 	}
+	
 
 	public ResponseEntity<List<RoleCapacity>> getTeamRoleCapacitySummary(UUID teamId, LocalDate startDate, LocalDate endDate) {
 		List<RoleCapacity> roleCapacityList = Collections.synchronizedList(new ArrayList<>());
@@ -94,51 +83,86 @@ public class TeamCapacityService {
 			.forEach(teamMemberCapacity -> {
 				List<MonthlyCapacity> monthlyCapacityList = null;
 				List<MonthlyCapacity> monthlyTeamCapacityList = null;
-				Boolean monthlyRoleCapacityAdded = false;
-				Boolean monthlyTeamCapacityAdded = false;
-
-				for (RoleCapacity roleCapacity : roleCapacityList) {
-					if (roleCapacity.getRole().equals(teamMember.getRole())) {
-						monthlyCapacityList = roleCapacity.getMonthlyCapacity();
-					}
-					
-					if(roleCapacity.getRole().equals(Role.TEAM)) {
-						monthlyTeamCapacityList = roleCapacity.getMonthlyCapacity();
-					}
-				}
-
-				for (MonthlyCapacity capacity : monthlyCapacityList) {
-					if (capacity.getDate().getMonth().equals(teamMemberCapacity.getDate().getMonth()) && capacity.getDate().getYear() == teamMemberCapacity.getDate().getYear()) {
-						capacity.setHours(capacity.getHours().add(teamMemberCapacity.getHours()));
-						monthlyRoleCapacityAdded = true;
-					}
-				}
-
-				if (Boolean.FALSE.equals(monthlyRoleCapacityAdded)) {
-					MonthlyCapacity monthlyCapacity = new MonthlyCapacity();
-					monthlyCapacity.setHours(teamMemberCapacity.getHours());
-					monthlyCapacity.setDate(teamMemberCapacity.getDate().minusDays(teamMemberCapacity.getDate().getDayOfMonth()-1));
-
-					monthlyCapacityList.add(monthlyCapacity);
+				
+				monthlyCapacityList = getMonthlyCapacityByRole(roleCapacityList, teamMember.getRole());
+				monthlyTeamCapacityList = getMonthlyCapacityByRole(roleCapacityList, Role.TEAM);
+				
+				if(null == monthlyCapacityList) {
+					throw new CapacityProcessingException(String.format("Exception encountered when trying to build a capacity report for role, %s", teamMember.getRole().toString()));
 				}
 				
-				for(MonthlyCapacity capacity : monthlyTeamCapacityList) {
-					if (capacity.getDate().getMonth().equals(teamMemberCapacity.getDate().getMonth()) && capacity.getDate().getYear() == teamMemberCapacity.getDate().getYear()) {
-						capacity.setHours(capacity.getHours().add(teamMemberCapacity.getHours()));
-						monthlyTeamCapacityAdded = true;
-					}
+				if(null == monthlyTeamCapacityList) {
+					throw new CapacityProcessingException(String.format("Exception encountered when trying to build a capacity report for role, %s", Role.TEAM.toString()));
 				}
-				
-				if(Boolean.FALSE.equals(monthlyTeamCapacityAdded)) {
-					MonthlyCapacity monthlyCapacity = new MonthlyCapacity();
-					monthlyCapacity.setHours(teamMemberCapacity.getHours());
-					monthlyCapacity.setDate(teamMemberCapacity.getDate().minusDays(teamMemberCapacity.getDate().getDayOfMonth()-1));
-					
-					monthlyTeamCapacityList.add(monthlyCapacity);
-				}
+
+
+				addCapacityToMonthlyRoleCapacity(monthlyCapacityList, teamMemberCapacity);
+				addCapacityToMonthlyRoleCapacity(monthlyTeamCapacityList, teamMemberCapacity);
 			}));
 
 		return new ResponseEntity<>(roleCapacityList, HttpStatus.OK);
+	}
+	
+	public List<DailyCapacity> getDailyCapacityByRole(List<RoleCapacity> roleCapacityList, Role role) {
+		List<DailyCapacity> dailyCapacityList = null;
+		
+		for (RoleCapacity roleCapacity : roleCapacityList) {
+			if (roleCapacity.getRole().equals(role)) {
+				dailyCapacityList = roleCapacity.getDailyCapacity();
+			}
+		}
+		
+		return dailyCapacityList;
+	}
+	
+	public List<MonthlyCapacity> getMonthlyCapacityByRole(List<RoleCapacity> roleCapacityList, Role role) {
+		List<MonthlyCapacity> monthlyCapacityList = null;
+		
+		for (RoleCapacity roleCapacity : roleCapacityList) {
+			if (roleCapacity.getRole().equals(role)) {
+				monthlyCapacityList = roleCapacity.getMonthlyCapacity();
+			}
+		}
+		
+		return monthlyCapacityList;
+	}
+	
+	public void addCapacityToDailyRoleCapacity(List<DailyCapacity> dailyCapacityList, TeamMemberCapacity teamMemberCapacity) {
+		Boolean dailyCapacityAdded = false;
+		
+		for (DailyCapacity capacity : dailyCapacityList) {
+			if (capacity.getDate().isEqual(teamMemberCapacity.getDate())) {
+				capacity.setHours(capacity.getHours().add(teamMemberCapacity.getHours()));
+				dailyCapacityAdded = true;
+			}
+		}
+
+		if (Boolean.FALSE.equals(dailyCapacityAdded)) {
+			DailyCapacity dailyCapacity = new DailyCapacity();
+			dailyCapacity.setHours(teamMemberCapacity.getHours());
+			dailyCapacity.setDate(teamMemberCapacity.getDate());
+
+			dailyCapacityList.add(dailyCapacity);
+		}
+	}
+	
+	public void addCapacityToMonthlyRoleCapacity(List<MonthlyCapacity> monthlyCapacityList, TeamMemberCapacity teamMemberCapacity) {
+		Boolean monthlyRoleCapacityAdded = false;
+		
+		for (MonthlyCapacity capacity : monthlyCapacityList) {
+			if (capacity.getDate().getMonth().equals(teamMemberCapacity.getDate().getMonth()) && capacity.getDate().getYear() == teamMemberCapacity.getDate().getYear()) {
+				capacity.setHours(capacity.getHours().add(teamMemberCapacity.getHours()));
+				monthlyRoleCapacityAdded = true;
+			}
+		}
+
+		if (Boolean.FALSE.equals(monthlyRoleCapacityAdded)) {
+			MonthlyCapacity monthlyCapacity = new MonthlyCapacity();
+			monthlyCapacity.setHours(teamMemberCapacity.getHours());
+			monthlyCapacity.setDate(teamMemberCapacity.getDate().minusDays(teamMemberCapacity.getDate().getDayOfMonth()-1));
+
+			monthlyCapacityList.add(monthlyCapacity);
+		}
 	}
 
 }
---
+++ src/main/java/tech/swayzetrain/capacity/common/exception/CapacityProcessingException.java
@@ -0,0 +1,11 @@
+package tech.swayzetrain.capacity.common.exception;
+
+public class CapacityProcessingException extends RuntimeException {
+
+	private static final long serialVersionUID = 4487455012179460462L;
+	
+	public CapacityProcessingException(String message) {
+		super(message);
+	}
+
+}
